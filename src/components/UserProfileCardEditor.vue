e5d90bfa6f327203e13a78d7564214d555d1866b
Kyle Cearley <kyle@ractoon.com>
2018-06-22 15:26:07 -0600

    Initial commit

---
+++ src/components/UserProfileCardEditor.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="profile-card">
+      <p class="text-center">
+        <img :src="user.avatar" alt="" class="avatar-xlarge img-update">
+      </p>
+
+      <div class="form-group">
+        <input type="text" v-model="activeUser.username" placeholder="Username" class="form-input text-lead text-bold">
+      </div>
+
+      <div class="form-group">
+        <input type="text" v-model="activeUser.name" placeholder="Full Name" class="form-input text-lead">
+      </div>
+
+      <div class="form-group">
+        <label for="user_bio">Bio</label>
+        <textarea v-model="activeUser.bio" class="form-input" id="user_bio" placeholder="Write a few words about yourself."></textarea>
+      </div>
+
+      <div class="stats">
+        <span>{{userPostsCount}} posts</span>
+        <span>{{userThreadsCount}} threads</span>
+      </div>
+
+      <hr>
+
+      <div class="form-group">
+        <label class="form-label" for="user_website">Website</label>
+        <input v-model="activeUser.website" autocomplete="off" class="form-input" id="user_website">
+      </div>
+
+      <div class="form-group">
+        <label class="form-label" for="user_email">Email</label>
+        <input v-model="activeUser.email" autocomplete="off" class="form-input" id="user_email">
+      </div>
+
+      <div class="form-group">
+        <label class="form-label" for="user_location">Location</label>
+        <input v-model="activeUser.location" autocomplete="off" class="form-input" id="user_location">
+      </div>
+
+      <div class="btn-group space-between">
+        <button @click.prevent="cancel" class="btn-ghost">Cancel</button>
+        <button @click.prevent="save" type="submit" class="btn-blue">Save</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      required: true,
+      type: Object
+    },
+    userPostsCount: {
+      required: true,
+      type: Number
+    },
+    userThreadsCount: {
+      required: true,
+      type: Number
+    }
+  },
+
+  data () {
+    return {
+      activeUser: {...this.user}
+    }
+  },
+
+  methods: {
+    save () {
+      this.$store.dispatch('updateUser', {...this.activeUser})
+      this.$router.push({name: 'Profile'})
+    },
+
+    cancel () {
+      this.$router.push({name: 'Profile'})
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import AppDate from '@/components/AppDate'
+import store from '@/store'
+
+Vue.component('AppDate', AppDate)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App }
+})
